7f8c247a217ad104ef8d81d45fa70a0ea8c92e3d
Xan-0 <johanalexis650@gmail.com>
2025-05-22 19:02:45 -0400

    Delete Silent Strikers (Alfa Muy temprana) directory

--- Silent Strikers (Alfa Muy temprana)/Codigos/visionGuardia.gd
+++
@@ -1,9 +0,0 @@
-extends RayCast2D
-
-var angle_cone_of_vision = 30.0
-var angle_between_rays = 5
-
-func _physics_process(delta: float) -> void:
-	if is_colliding():
-		if get_collider().name == "Ladron":
-			print("PLayer detetcted")
