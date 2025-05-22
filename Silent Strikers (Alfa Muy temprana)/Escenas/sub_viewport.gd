2822d7c0bdf098231345d287ff034d612a66b9c1
Garcia-IGC <ignacio.garcia02@alumnos.ucn.cl>
2025-05-22 17:37:37 -0400

    HUD minimapa con fondo actualizado

--- Silent Strikers (Alfa Muy temprana)/Escenas/sub_viewport.gd
+++ Silent Strikers (Alfa Muy temprana)/Escenas/sub_viewport.gd
@@ -1,8 +1,10 @@
 extends SubViewport
 
-@onready var minimap_camera = $Camera2D
-@onready var player = get_node("/root/Mapa de Prueba/Ladron")  # Cambiá esta ruta si es diferente
+@onready var minimap_camera: Camera2D = $Camera2D
+@onready var player: Node2D = get_node("/root/Mapa de Prueba/Ladron")  # Ajustá si el path es otro
+@onready var mapa: TileMapLayer = $TileMapLayer  # Tu copia visual del mapa
 
 func _process(_delta):
 	if player and minimap_camera:
+		# Hace que la cámara del minimapa siga al jugador
 		minimap_camera.global_position = player.global_position
