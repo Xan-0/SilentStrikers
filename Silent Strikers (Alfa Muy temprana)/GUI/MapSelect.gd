2b9afa6803a6bce37dc8c3b2e5dfe7d67b69e989
Garcia-IGC <ignacio.garcia02@alumnos.ucn.cl>
2025-07-06 17:04:04 -0400

    Separacion de mapas
    Separacion de mapas y seleccion de estos mismos en diferentes mapas, y seleccion

--- Silent Strikers (Alfa Muy temprana)/GUI/MapSelect.gd
+++ Silent Strikers (Alfa Muy temprana)/GUI/MapSelect.gd
@@ -11,11 +11,24 @@ func _process(delta):
 	shader_material.set_shader_parameter("flicker_value", flicker)
 
 
-func _on_Mapa_1_pressed() -> void:
-	var mapa_scene = load("res://Escenas/Mansion.tscn")
-	get_tree().change_scene_to_packed(mapa_scene)
+
 
 
 func _on_go_back_button_pressed() -> void:
 	var mapa_scene = load("res://Escenas/main_menu.tscn")
+	get_tree().change_scene_to_packed(mapa_scene)
+
+
+
+func _on_map_2_button_pressed() -> void:
+	var mapa_scene = load("res://Escenas/Museo.tscn")
+	get_tree().change_scene_to_packed(mapa_scene)
+
+func _on_map_3_button_pressed() -> void:
+	var mapa_scene = load("res://Escenas/Banco.tscn")
+	get_tree().change_scene_to_packed(mapa_scene)
+
+
+func _on_map_1_button_2_pressed() -> void:
+	var mapa_scene = load("res://Escenas/Mansion.tscn")
 	get_tree().change_scene_to_packed(mapa_scene)
--- Silent Strikers (Alfa Muy temprana)/GUI/MapSelectMultijugador.gd
+++ Silent Strikers (Alfa Muy temprana)/GUI/MapSelectMultijugador.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Control
 @onready var shader_material: ShaderMaterial = $LightMask.material
 var time := 0.0
 
@@ -7,23 +7,25 @@ func _process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 	shader_material.set_shader_parameter("light_position", mouse_pos)
 
-	# Flicker suave con aleatoriedad
 	var flicker = sin(time * 6.0) * 0.5 + randf_range(-0.2, 0.2)
 	shader_material.set_shader_parameter("flicker_value", flicker)
-	
 
 
-
-func _on_singleplayer_button_pressed() -> void:
-	var mapa_scene = load("res://Escenas/map_select.tscn")
+func _on_Mapa_1_pressed() -> void:
+	var mapa_scene = load("res://GUI/Escenas/Mansion_multijugador.tscn")
 	get_tree().change_scene_to_packed(mapa_scene)
 
 
-func _on_multiplayer_button_pressed() -> void:
-	var mapa_scene = load("res://Escenas/multiplayer_scene.tscn")
+func _on_go_back_button_pressed() -> void:
+	var mapa_scene = load("res://Escenas/main_menu.tscn")
 	get_tree().change_scene_to_packed(mapa_scene)
 
 
-func _on_options_button_pressed() -> void:
-	var mapa_scene = load("res://Escenas/settings_screen.tscn")
+func _on_mapa_2_pressed() -> void:
+	var mapa_scene = load("res://GUI/Escenas/Museo_multijugador.tscn")
+	get_tree().change_scene_to_packed(mapa_scene)
+
+
+func _on_mapa_3_pressed() -> void:
+	var mapa_scene = load("res://GUI/Escenas/Banco_multijugador.tscn")
 	get_tree().change_scene_to_packed(mapa_scene)
